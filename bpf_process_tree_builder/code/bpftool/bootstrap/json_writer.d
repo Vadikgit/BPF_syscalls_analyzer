7c90f486824992ce2f19a944b01a1365662a1d7d
Vadikgit <ksenofontovvs9a@mail.ru>
2025-04-24 13:39:33 +0300

    Bpf-specific libs and headers made shared. Older versions deleted

--- bpf_process_tree_builder/code/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/home/vadim/Документы/prog/btstrp/libbpf-bootstrap/examples/c/.output/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /home/vadim/Документы/prog/btstrp/libbpf-bootstrap/bpftool/include/linux/compiler.h \
- /home/vadim/Документы/prog/btstrp/libbpf-bootstrap/bpftool/include/linux/compiler_types.h \
- /home/vadim/Документы/prog/btstrp/libbpf-bootstrap/bpftool/include/linux/compiler-gcc.h
